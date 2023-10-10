3e135a46c4c0aff5aa0b8868c2c84a0580d1de05
Olav Bjørlykke <olavbjorlykke@dhcp-10-22-58-218.wlan.ntnu.no>
2023-10-10 13:58:24 +0200

    Added test model and data files

---
+++ facility.sol
@@ -0,0 +1,27 @@
+# Solution for model facility
+# Objective value = 210500
+Open0 1
+Open1 1
+Open2 0
+Open3 1
+Open4 1
+Trans0.0 0
+Trans1.0 14
+Trans2.0 0
+Trans3.0 1
+Trans4.0 0
+Trans0.1 0
+Trans1.1 0
+Trans2.1 0
+Trans3.1 18
+Trans4.1 0
+Trans0.2 14
+Trans1.2 0
+Trans2.2 0
+Trans3.2 0
+Trans4.2 0
+Trans0.3 6
+Trans1.3 8
+Trans2.3 0
+Trans3.3 0
+Trans4.3 6
